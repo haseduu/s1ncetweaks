a1a4c9f512fa9b95e73b01191c77afa073dbf6b3
Eduardo Hasegawa <159029556+haseduu@users.noreply.github.com>
2025-05-01 01:29:32 -0700

    Update s1ncetweaks.bat

--- s1ncetweaks.bat
+++ s1ncetweaks.bat
@@ -83,9 +83,9 @@ echo.
 echo [1/3] OOSU10.exe (for privacy settings)
 curl -L --silent -o "%s1nce%\OOSU10.exe" "https://dl5.oo-software.com/files/ooshutup10/OOSU10.exe"
 echo [2/3] ooshutup10.cfg (configuration file)
-curl -L --silent -o "%s1nce%\ooshutup10.cfg" "https://github.com/haseduu/s1ncetweaks/blob/main/resources/ooshutup10.cfg"
+curl -L --silent -o "%s1nce%\ooshutup10.cfg" "https://raw.githubusercontent.com/haseduu/s1ncetweaks/main/resources/ooshutup10.cfg"
 echo [3/3] Khorvie.pow (powerplan for max performance)
-curl -L --silent -o "C:\Khorvie.pow" "https://github.com/haseduu/s1ncetweaks/blob/main/resources/Khorvie.pow"
+curl -L --silent -o "C:\Khorvie.pow" "hhttps://raw.githubusercontent.com/haseduu/s1ncetweaks/main/resources/Khorvie.pow"
 echo.
 echo Download completed successfully!
 echo.
